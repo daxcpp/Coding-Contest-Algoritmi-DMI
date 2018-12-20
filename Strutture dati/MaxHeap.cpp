9f4e343719b1239037313bbd0b3355aee6fca008
daxcpp <cdavide98carnemolla@gmail.com>
2018-12-21 00:15:07 +0100

    Add MaxHeap

--- Strutture dati/MaxHeap.cpp
+++ Strutture dati/MaxHeap.cpp
@@ -1,13 +1,13 @@
-//MinHeap
 #include <iostream>
 using namespace std;
 
+
 template <class H>
 class Heap{
-protected:
+private:
     H* vett;
-    int heapsize;
     int maxdim;
+    int heapsize;
     int chiamate;
 
     int left(int i){return i << 1;}
@@ -18,11 +18,9 @@ public:
         this->maxdim = maxdim;
         this->heapsize = 0;
         this->chiamate = 0;
-        vett = new H[maxdim+1];
+        this->vett = new H[maxdim+1];
     }
-
     int getChiamate(){return chiamate;}
-
     void Heapify(int);
     Heap<H>* Enqueue(H);
     H Extract();
@@ -32,34 +30,34 @@ public:
 };
 
 template <class H> void Heap<H>::Heapify(int i){
-    if(heapsize >= 1)
+    if(heapsize >= 0)
         chiamate++;
-    
     int l = left(i);
     int r = right(i);
-    int min = i;
+    int max = i;
 
-    if(l <= heapsize && vett[l] < vett[min])
-        min = l;
-    if(r <= heapsize && vett[r] < vett[min])
-        min = r;
+    if(l <= heapsize && vett[l] > vett[max])
+        max = l;
+    if(r <= heapsize && vett[r] < vett[max])
+        max = r;
     
-    if(min != i){
-        swap(vett[min], vett[i]);
-        Heapify(min);
+    if(max != i){
+        swap(vett[max], vett[i]);
+        Heapify(max);
     }
 }
 
 template <class H> Heap<H>* Heap<H>::Enqueue(H x){
     heapsize++;
     vett[heapsize] = x;
-
+    
     int i = heapsize;
 
-    while(i > 0 && vett[parent(i)] > vett[i]){
+    while(i > 1 && vett[parent(i)] < vett[i]){
         swap(vett[parent(i)], vett[i]);
         i = parent(i);
     }
+    
     return this;
 }
 
@@ -72,23 +70,27 @@ template <class H> H Heap<H>::Extract(){
 }
 
 template <class H> void Heap<H>::Build(H* vett, int n){
-    heapsize = n;
     this->vett = vett;
+    this->heapsize = n;
 
     for(int i = heapsize/2; i > 0; i--)
         Heapify(i);
+    
 }
 
 template <class H> void Heap<H>::Empty(){
-    while(heapsize != 0)
+    while(heapsize != 0){
         this->Extract();
+    }
 }
 
 template <class H> void Heap<H>::Print(){
-    for(int i = 1; i < heapsize+1; i++)
+    for(int i = 1; i < heapsize + 1; i++)
         cout << vett[i] << "\t";
     cout << endl;
 }
+
+
 
 int main(){
     Heap<int>* t = new Heap<int>(10);
@@ -97,6 +99,4 @@ int main(){
     t->Print();
     t->Empty();
     t->Print();
-
-
 }
