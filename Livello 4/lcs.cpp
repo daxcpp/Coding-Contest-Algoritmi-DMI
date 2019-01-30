c8b437fa467e5a0d51ee89a6d4413e5bf3a4e3be
daxcpp <cdavide98carnemolla@gmail.com>
2019-01-30 17:15:53 +0100

    Update lcs.cpp

--- Livello 4/lcs.cpp
+++ Livello 4/lcs.cpp
@@ -14,10 +14,8 @@ void LCSlenght(string x , string y, int m, int n, ofstream &out){
         for(int j = 1; j < n+1; j++){
             if(x[i-1] == y[j-1])
                 C[i][j] = C[i-1][j-1]+1;
-            else if(C[i-1][j] >= C[i][j-1])
-                C[i][j] = C[i-1][j];
             else
-                C[i][j] = C[i][j-1];
+            	C[i][j] = max(C[i-1][j], C[i][j-1]);
         }
     }
 
